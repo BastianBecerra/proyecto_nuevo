3eee9a52763d82068ee7ff17c424d1237f4891b6
Bastian Becerra <bastianbecerrabasoalto@gmail.com>
2024-06-02 15:12:21 -0400

    agregado de secciones, ademas de una mini informacion la cual cambiare despues.

---
+++ src/componentes/pagina_principal/infoGeneral2.jsx
@@ -0,0 +1,51 @@
+const InfoGeneral2 = () => {
+    return(
+        <section>
+            <div>
+                <h1>Info 2</h1>
+            </div>
+
+            <div>
+                <div className="">
+                    <div>
+                        <img src="" alt="" />
+                        <p>Logro 1: </p>
+
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                        <p>Logro 1: </p>
+
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                        <p>Logro 1: </p>
+
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                        <p>Logro 1: </p>
+
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                        <p>Logro 1: </p>
+
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                        <p>Logro 1: </p>
+
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                        <p>Logro 1: </p>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default InfoGeneral2;
--- src/componentes/pagina_principal/paginaPrincipal.jsx
+++ src/componentes/pagina_principal/paginaPrincipal.jsx
@@ -1,9 +1,13 @@
 import Banner from '../pagina_principal/banner.jsx'
+import InfoGeneral1 from './infoGeneral1.jsx';
+import InfoGeneral2 from './infoGeneral2.jsx';
 
 const PaginaPrincipal = () => {
     return (
         <section>
             <Banner/>
+            <InfoGeneral1/>
+            <InfoGeneral2/>
         </section>
     )
 }
